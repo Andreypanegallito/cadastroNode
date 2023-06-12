93e54b41aff42ca2b203929ae52ad68c2a4f862b
Andrey Lucas Machado Panegalli <andreigd20@gmail.com>
2023-06-11 22:29:01 -0300

    Ajustado algumas coisas na tabela

--- client/src/pages/users/components/table/index.tsx
+++ client/src/pages/users/components/table/index.tsx
@@ -13,21 +13,26 @@ function UserTable({ users }: UserTable) {
         <tr>
           <th>ID</th>
           <th>Nome</th>
-          <th>Sobrenome</th>
+          {/* <th>Sobrenome</th> */}
           <th>Email</th>
           <th>Data de criação</th>
+          <th>Opções</th>
         </tr>
       </thead>
       <tbody>
         {
           users.length > 0 ? (
             users.map(user => (
-              <tr key={user.idUsuario}>
+              <tr key={user.idUsuario} className={user.idUsuario % 2 === 0 ? 'even' : 'odd'}>
                 <td className="id">{user.idUsuario}</td>
                 <td className="name">{user.nome} {user.sobrenome}</td>
                 {/* <td >{user.sobrenome}</td> */}
                 <td className='email'>{user.email}</td>
                 <td className='data_criacao'>{user.data_criacao.toLocaleString()}</td>
+                <td className='opcoes'>
+                  <button>Editar</button>
+                  <button>Apagar</button>
+                </td>
               </tr>
             ))
           ) : (
--- server/dist/services/userService.js
+++ server/dist/services/userService.js
@@ -3,11 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUser = exports.getUserById = void 0;
+exports.loginUser = exports.createUser = exports.getUserById = exports.getAllUsers = void 0;
 const db_1 = __importDefault(require("../database/db"));
+const getAllUsers = () => {
+    return new Promise((resolve, reject) => {
+        db_1.default.query("SELECT idUsuario, nome, sobrenome, username, email, DATE_FORMAT(data_criacao, '%d-%m-%Y %H:%i:%s') as data_criacao FROM USUARIOS", (error, results) => {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve(results);
+            }
+        });
+    });
+};
+exports.getAllUsers = getAllUsers;
 const getUserById = (userId) => {
     return new Promise((resolve, reject) => {
-        db_1.default.query("SELECT * FROM users WHERE id = ?", [userId], (error, results) => {
+        db_1.default.query("SELECT * FROM usuarios WHERE idUsuario = ?", [userId], (error, results) => {
             if (error) {
                 reject(error);
             }
@@ -20,7 +33,7 @@ const getUserById = (userId) => {
 exports.getUserById = getUserById;
 const createUser = (user) => {
     return new Promise((resolve, reject) => {
-        db_1.default.query("INSERT INTO users SET ?", user, (error, result) => {
+        db_1.default.query("INSERT INTO usuarios SET ?", user, (error, result) => {
             if (error) {
                 reject(error);
             }
@@ -31,4 +44,23 @@ const createUser = (user) => {
     });
 };
 exports.createUser = createUser;
+const loginUser = (userName, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = `select * from usuarios where username = '${userName}' and password = '${password}'`;
+        db_1.default.query(sql, (error, result) => {
+            if (error) {
+                reject(error);
+            }
+            else {
+                const status = "Ok";
+                const loginResponse = {
+                    result: result,
+                    status,
+                };
+                resolve(loginResponse);
+            }
+        });
+    });
+};
+exports.loginUser = loginUser;
 // Outros métodos relacionados a consultas de usuários...
