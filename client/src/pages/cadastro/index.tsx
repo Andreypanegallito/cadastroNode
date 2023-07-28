90b6b168b8a6cf1b11fbd409b31de8e390704853
Andreypanegallito <andrey.panegalli@gmail.com>
2023-07-28 12:37:51 -0300

    Ajustes

--- client/src/pages/cadastro/index.tsx
+++ client/src/pages/cadastro/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CadastroUsuario from './components';
 
+import './cadastro.scss';
+
 const Cadastro = () => {
   return (
-    <section >
+    <section id="sectionCadastroUsuarios">
       <div className="titulo">
         <h1>Cadastro de usuário</h1>
       </div>
