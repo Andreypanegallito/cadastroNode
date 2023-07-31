9906012c90b7864d0fc63fe7a182d2643cf9faab
Andrey Lucas Machado Panegalli <andreigd20@gmail.com>
2023-07-31 00:36:07 -0300

    Retirado a senha temporária e adicionada no dotenv

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "@Andreyalmp10",
-  database: "dbcadastro",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((error) => {
