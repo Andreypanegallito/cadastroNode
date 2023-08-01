a399186c53573b8e3136a4136205c4f8f648b4a6
Andrey Lucas Machado Panegalli <andreigd20@gmail.com>
2023-08-01 20:38:27 -0300

    Ajustando a conexao com o banco de dados online

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+console.log(process.env.DATABASE_URL);
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((error) => {
   if (error) {
@@ -17,5 +20,6 @@ connection.connect((error) => {
     console.log("Conexão estabelecida com o banco de dados.");
   }
 });
+// connection.end();
 
 export default connection;
