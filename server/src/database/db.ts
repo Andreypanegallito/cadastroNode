e062169590c1c7a84a5bfb6a5fb59d6511b15fa1
Andreypanegallito <andrey.panegalli@gmail.com>
2023-08-02 12:38:49 -0300

    Retirado comentários.

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -3,12 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
 console.log(process.env.DATABASE_URL);
 
 const connection = mysql.createConnection(process.env.DATABASE_URL);
