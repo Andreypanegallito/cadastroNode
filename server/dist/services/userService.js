e8a7aa81aeb14d05527796ba9130bda844155456
Andrey Lucas Machado Panegalli <andreigd20@gmail.com>
2023-08-15 22:39:52 -0300

    Atualizado código js gerado

--- server/dist/services/userService.js
+++ server/dist/services/userService.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.loginUser = exports.deleteUser = exports.updateUser = exports.createUser = exports.getUserById = exports.getAllUsers = void 0;
 const db_1 = __importDefault(require("../database/db"));
+const bcrypt = require("bcrypt");
 const getAllUsers = () => {
     return new Promise((resolve, reject) => {
         db_1.default.query("SELECT idUsuario, nome, sobrenome, username, email, DATE_FORMAT(data_criacao, '%d-%m-%Y %H:%i:%s') as data_criacao, ativo FROM usuarios", (error, results) => {
@@ -33,13 +34,20 @@ const getUserById = (userId) => {
 exports.getUserById = getUserById;
 const createUser = (user) => {
     return new Promise((resolve, reject) => {
-        db_1.default.query("INSERT INTO usuarios SET ?", user, (error, result) => {
-            if (error) {
-                reject(error);
+        bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+            if (err) {
+                reject(err);
+                return;
             }
-            else {
-                resolve("Ok");
-            }
+            const newUser = { ...user, password: hashedPassword };
+            db_1.default.query("INSERT INTO usuarios SET ?", newUser, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve("Ok");
+                }
+            });
         });
     });
 };
@@ -72,18 +80,42 @@ const deleteUser = (idUsuario) => {
 exports.deleteUser = deleteUser;
 const loginUser = (userName, password) => {
     return new Promise((resolve, reject) => {
-        const sql = `select * from usuarios where username = '${userName}' and password = '${password}'`;
-        db_1.default.query(sql, (error, result) => {
+        const sql = `SELECT * FROM usuarios WHERE username = ?`;
+        db_1.default.query(sql, [userName], async (error, results) => {
             if (error) {
                 reject(error);
             }
             else {
-                const status = "Ok";
-                const loginResponse = {
-                    result: result,
-                    status,
-                };
-                resolve(loginResponse);
+                const result = results[0];
+                if (!result) {
+                    // Usuário não encontrado
+                    const status = "userErr";
+                    const loginResponse = {
+                        result: null,
+                        status,
+                    };
+                    resolve(loginResponse);
+                    return;
+                }
+                const storedHashedPassword = result.password;
+                const passwordsMatch = await bcrypt.compare(password, storedHashedPassword);
+                if (passwordsMatch) {
+                    const status = "Ok";
+                    const loginResponse = {
+                        result: result,
+                        status,
+                    };
+                    resolve(loginResponse);
+                }
+                else {
+                    // senha errada
+                    const status = "passErr";
+                    const loginResponse = {
+                        result: null,
+                        status,
+                    };
+                    resolve(loginResponse);
+                }
             }
         });
     });
