773eb42c0250448f5201587b6b7dac7bc6f751a7
Andrey Panegalli <andrey.panegalli@gmail.com>
2023-08-31 13:31:45 -0300

    Ajuste de formatação

--- client/src/pages/initial/index.tsx
+++ client/src/pages/initial/index.tsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import './initial.scss'
-import Cookies from 'js-cookie';
+import "./initial.scss";
+import Cookies from "js-cookie";
 
 const Initial = () => {
   const navigate = useNavigate();
-  const deslogar = () => {
-    Cookies.remove('jwtToken');
 
-    navigate('/login');
+  const deslogar = () => {
+    Cookies.remove("jwtToken");
+
+    navigate("/login");
   };
+
   return (
-    <section id='initial'>
+    <section id="initial">
       <div className="itens">
         <div className="item">
-          <button className='btn-link'>
-            <Link to="/users" >Usuários</Link>
+          <button className="btn-link">
+            <Link to="/users">Usuários</Link>
           </button>
         </div>
         <div className="item">
-          <button className='btn-link'>
+          <button className="btn-link">
             <Link to="/cadastro">Cadastro</Link>
           </button>
         </div>
         <div className="item">
-          <button className='btn-link'>
+          <button className="btn-link">
             <Link to="/contato">Contato</Link>
           </button>
         </div>
         <div className="item">
-          <button onClick={deslogar} className='btn-link'>Deslogar</button>
+          <button onClick={deslogar} className="btn-link">
+            Deslogar
+          </button>
         </div>
       </div>
     </section>
